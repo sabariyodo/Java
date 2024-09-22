388940ae42763f41939553788bf6b0f3a1d5737b
user <user@Sabari>
2024-09-22 15:23:10 +0530

    Palindrome

--- boltz1/src/main/java/string/Remove_WhiteSpace.java
+++ boltz1/src/main/java/string/Remove_WhiteSpace.java
@@ -1,24 +1,24 @@
 package string;
 
 public class Remove_WhiteSpace {
-	 static String rem="";
+	  String rem="";
 
 	
 	
 //Using replaceAll method
-	  static String removeAllspace(String str){
+	   String removeAllspace(String str){
 		 rem = str.replaceAll("\\s", "");
 		return rem;
 	}
 	 
 //Using replace
-	 static String replace(String str) {
+	  String replace(String str) {
 		 rem = str.replace(" ", "");
 		 return rem; 
 	 }
 
 //Using char
-	 static String charmethod (String str) {
+	  String charmethod (String str) {
 		 char[] out = str.toCharArray();
 		 for(char ch: out) {
 			 if(!Character.isWhitespace(ch)) {
@@ -32,10 +32,13 @@ public class Remove_WhiteSpace {
 	public static void main(String[] args) {
 		String str = "Java Developer ";
 		String str1=" J a v a D e v e l o p e r";
+		
+		Remove_WhiteSpace rw = new Remove_WhiteSpace();
+		
 		System.out.println(str);
-		System.out.println(removeAllspace(str));
-		System.out.println(replace(str1));
-		System.out.println(charmethod(str1));
+		System.out.println(rw.removeAllspace(str));
+		System.out.println(rw.replace(str1));
+		System.out.println(rw.charmethod(str1));
 	}
 
 }
