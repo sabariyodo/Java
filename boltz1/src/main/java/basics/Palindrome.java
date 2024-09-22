746dc46d8c875de5f5753fdc2e52b5ca2eb38a06
user <user@Sabari>
2024-09-22 15:23:53 +0530

    Palindrome

---
+++ boltz1/src/main/java/basics/Palindrome.java
@@ -0,0 +1,22 @@
+package basics;
+
+import java.util.Iterator;
+
+public class Palindrome {
+	
+	public static void main(String[] args) {
+		
+		int a=121;
+		int rev=0,rem = 0;
+		
+		for (int i = 3; i>3; i--) {
+			rem = a/10;	
+			rev+=rem;
+		}
+		System.out.println(rem);
+			
+		
+	}
+	
+
+}
---
+++ boltz1/src/main/java/string/Palindrome.java
@@ -0,0 +1,11 @@
+package string;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
