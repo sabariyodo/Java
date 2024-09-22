10fb4ef5aa40fc85c0ddc10ba3c540862e059c27
user <user@Sabari>
2024-09-22 15:38:31 +0530

    Immutable program

---
+++ boltz1/src/main/java/string/ImmutableString.java
@@ -0,0 +1,16 @@
+package string;
+
+public class ImmutableString {
+	 public static void main(String[] args) {
+		String s1 = "java", s2 = "Developers";
+		String s3;
+		
+		s3 = s1+s2;
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		
+	}
+
+}
