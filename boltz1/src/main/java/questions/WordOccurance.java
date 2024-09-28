9144d6cc28c03671b3902c7fa350cb6756d18d93
user <user@Sabari>
2024-09-28 11:57:33 +0530

    word occurance

--- boltz1/src/main/java/questions/WordOccurance.java
+++ boltz1/src/main/java/questions/WordOccurance.java
@@ -1,31 +1,37 @@
 package questions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class WordOccurance {
 	int wordcount = 1;
 	String word, nextword;
 
 	String occ(String str) {
 		System.out.println(str);
-		String[] st = str.split(" ");
-		for (int i = 0; i < st.length; i++) {
-			word = st[i];
-			for (int j = 1; j < i; j++) {
-				
-				nextword = st[j];
-			}
-			if (word.equalsIgnoreCase(nextword)) {
-				wordcount++;
-			}
-			System.out.println("word :" + st[i] + " - count : " + wordcount);
-			System.out.println(st[i]);
+		
+		//convert string to lowercase
+		str = str.toLowerCase().replaceAll("[^a-zA-Z ]", "");
+		
+		String[] words = str.split("\\s+");
+		
+		HashMap<String, Integer> map = new HashMap<>();
+		
+		for (String string : words) {
+			
+			map.put(string, map.getOrDefault(string,0)+1);
 		}
-
+		
+		for(Map.Entry<String, Integer> entry: map.entrySet()) {
+			System.out.println(entry.getKey()+":"+entry.getValue());
+		}
+		
 		return str;
 	}
 
 	public static void main(String[] args) {
 
-		String str = "I am Java developer i am proud of it";
+		String str = "I am Java developer55 i am proud of it";
 		WordOccurance st = new WordOccurance();
 		st.occ(str);
 
